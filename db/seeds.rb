dc0256c9c8a84519666d474b4ba5d7dfc4567fd3
remybigot <remybigot@gmail.com>
2023-08-31 14:12:43 +0200

    wednesday

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,137 @@
 puts "creation m..."
 
-match = Match.new(
-  comment: "blablablablablablab",
-  dev_id: 3,
-  mission_id: 12,
-  accepted: true
+user = User.new(
+  email: "tutu@gmail.com",
+  password: "123456",
+  name: "tutu",
+  company: "webcompany",
+  phone: "0456342345",
+  area: "tech",
+  location: "paris",
+  upvote: 2,
+  fav: "Excellent worker"
 )
-match.save!
+user.save!
 
-match1 = Match.new(
-  comment: "le cheval c est trop genial!",
-  dev_id: 2,
-  mission_id: 12,
-  accepted: true
+user = User.new(
+  email: "tata@gmail.com",
+  password: "123456",
+  name: "tata",
+  company: "Webagency",
+  phone: "0326311345",
+  area: "marketing",
+  location: "Lyon",
+  upvote: 1,
+  fav: "Amazingly done !"
 )
-match1.save!
+user.save!
 
-match2 = Match.new(
-  comment: "nnnnnnnnnnnnnnnnnnnnab",
-  dev_id: 1,
-  mission_id: 12,
-  accepted: true
+mission = Mission.new(
+  name: "Projet Danone",
+  area: "food",
+  location: "Paris",
+  description: "JS secret app for the US market",
+  price: "1000",
+  start_date: "01/01/2024",
+  end_date: "01/05/2024",
+  user_id: 6
 )
-match2.save!
+mission.save!
 
-match3 = Match.new(
-  comment: "blavjhvyvutvjgcjtcjb",
-  dev_id: 4,
-  mission_id: 12,
-  accepted: true
+mission = Mission.new(
+  name: "Projet Tesla",
+  area: "automotive",
+  location: "San Francisco",
+  description: "Python project to improve the cars",
+  price: "1500",
+  start_date: "01/03/2024",
+  end_date: "01/05/2024",
+  user_id: 6
 )
-match3.save!
+mission.save!
+
+# dev = Dev.new(
+#   name: "Johnny",
+#   email: "johnny@johnny.com",
+#   availability: true,
+#   password: "123456",
+#   phone: "0102030405",
+#   area: "agriculture",
+#   upvote: 10,
+#   location: "Marseille",
+#   diploma: "Le Wagon 2023",
+#   # soft_skill: ["teamwork", "stress control", "communication"],
+#   # language: ["Python", "HTML5"]
+# )
+# dev.save!
+# dev = Dev.new(
+#   name: "Lucy",
+#   email: "lucy@lucy.com",
+#   availability: true,
+#   password: "123456",
+#   phone: "0102030406",
+#   area: "energy",
+#   upvote: 45,
+#   location: "Lyon",
+#   diploma: "Le Wagon 2020",
+#   # soft_skill: ["autonomy", "teamwork", "communication"],
+#   # language: ["Python", "HTML5", "Ruby", "Javascript"]
+# )
+# dev.save!
+# dev = Dev.new(
+#   name: "Emily",
+#   email: "emily@emily.com",
+#   availability: true,
+#   password: "123456",
+#   phone: "0102030407",
+#   area: "healthcare",
+#   upvote: 4,
+#   location: "Marseille",
+#   diploma: "Le Wagon 2023",
+#   # soft_skill: ["adaptative", "stress control", "organised"],
+#   # language: ["Ruby", "HTML5"]
+# )
+# dev.save!
+# dev = Dev.new(
+#   name: "Anthony",
+#   email: "anthony@anthony.com",
+#   availability: true,
+#   password: "123456",
+#   phone: "0102030408",
+#   area: "communication",
+#   upvote: 10,
+#   location: "Paris",
+#   diploma: "Le Wagon 2019",
+#   # soft_skill: ["reactivity", "adaptative"],
+#   # language: ["Python", "Javascript", "Ruby"]
+# )
+# dev.save!
+
+# match = Match.new(
+#   comment: "Amazing",
+#   dev_id: 3,
+#   mission_id: 4,
+#   accepted: true
+# )
+# match.save!
+# match1 = Match.new(
+#   comment: "the best ever",
+#   dev_id: 2,
+#   mission_id: 5,
+#   accepted: true
+# )
+# match1.save!
+# match2 = Match.new(
+#   comment: "nobody moves like this",
+#   dev_id: 1,
+#   mission_id: 4,
+#   accepted: true
+# )
+# match2.save!
+# match3 = Match.new(
+#   comment: "perfecto",
+#   dev_id: 4,
+#   mission_id: 5,
+#   accepted: true
+# )
+# match3.save!
