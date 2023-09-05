5ba72e4b6700e0b3c86698b6a5eb4841f3fa1fc1
Lorenzo Lara <hola.enx@gmail.com>
2023-09-05 15:11:01 +0200

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_085909) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_04_140548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_085909) do
     t.string "twitter_link"
     t.string "facebook_link"
     t.string "insta_link"
-
   end
 
   create_table "devsoftmissions", force: :cascade do |t|
@@ -66,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_085909) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "langicon"
+    t.string "icon"
   end
 
   create_table "matches", force: :cascade do |t|
